a50378cdcda616678f0a8555b0b7a7397a83c9ae
Alice <a.andrianova@protoware.com>
2018-09-27 14:42:47 +0300

    Add prettier

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React, {Component} from 'react';
-import { View } from 'react-native';
-import firebase from 'firebase';
+import React, { Component } from "react";
+import { View } from "react-native";
+import firebase from "firebase";
 
-import { Header } from './components/common';
-import Loginform from './components/Loginform';
-
+import { Header } from "./components/common";
+import Loginform from "./components/Loginform";
 
 export default class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDCCR56-RKFvEiKn1mPSVk2u3WSaTgjNeM",
+      authDomain: "auth-e21ca.firebaseapp.com",
+      databaseURL: "https://auth-e21ca.firebaseio.com",
+      projectId: "auth-e21ca",
+      storageBucket: "auth-e21ca.appspot.com",
+      messagingSenderId: "520552945146"
+    });
+  }
 
-    componentWillMount() {
-        firebase.initializeApp({
-                apiKey: "AIzaSyDCCR56-RKFvEiKn1mPSVk2u3WSaTgjNeM",
-                authDomain: "auth-e21ca.firebaseapp.com",
-                databaseURL: "https://auth-e21ca.firebaseio.com",
-                projectId: "auth-e21ca",
-                storageBucket: "auth-e21ca.appspot.com",
-                messagingSenderId: "520552945146"
-            });
-    }
-
-    render() {
-        return(
-            <View>
-                <Header title="Authentication"/>
-                <Loginform />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View>
+        <Header title="Authentication" />
+        <Loginform />
+      </View>
+    );
+  }
 }
--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import React from "react";
+import { View, ActivityIndicator } from "react-native";
 
 const styles = {
-    spinnerStyle: {
-        flex: 1,
-        justifuContent: 'center',
-        alignItems: 'center'
-    }
-}
-
-const Spinner = ({size}) => {
-    return (
-        <View style={styles.spinnerStyle}>
-            <ActivityIndicator size={size || 'large'}/>
-        </View>
-    )
+  spinnerStyle: {
+    flex: 1,
+    justifuContent: "center",
+    alignItems: "center"
+  }
 };
 
-export { Spinner }
+const Spinner = ({ size }) => {
+  return (
+    <View style={styles.spinnerStyle}>
+      <ActivityIndicator size={size || "large"} />
+    </View>
+  );
+};
+
+export { Spinner };
